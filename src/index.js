d84f18bc86fb55bdcd559a90ab96623abd6177dd
Asprind <oh.u.slowpoke@gmail.com>
2020-10-01 00:35:47 +0300

    feat: made a functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
 exports.min = function min (array) {
-  return 0;
+	if(!array || array.length === 0) {
+        return 0;
+    	}
+	return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(!array || array.length === 0) {
+        return 0;
+    	}
+	return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(!array || array.length === 0) {
+        return 0;
+    	}
+	let count = 0;
+  	for (let i = 0; i < array.length; i++) {
+      	count += array[i];
+  	}
+  	return count / array.length;
 }
